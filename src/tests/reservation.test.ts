3b30e50e42f0d3c6bfb7886c89477ac4fa0b45ea
TheCosmicVibe <157511445+TheCosmicVibe@users.noreply.github.com>
2026-01-11 01:29:14 -0800

    Update reservation.test.ts

--- src/tests/reservation.test.ts
+++ src/tests/reservation.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
-import app from '../src/app';
-import { AppDataSource } from '../src/config/database';
-import { Restaurant } from '../src/models/Restaurant';
-import { Table } from '../src/models/Table';
+import app from '../../src/app';
+import { AppDataSource } from '../../src/config/database';
+import { Restaurant } from '../../src/models/Restaurant';
+import { Table } from '../../src/models/Table';
 
 const API_PREFIX = '/api/v1';
 
